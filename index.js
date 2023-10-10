e555a7ff7c2418a7c5f9af9bb65d36750975a4f0
Denis Kovylin <kovylin8519@gmail.com>
2023-10-10 17:59:04 +0300

    in progress 17:58

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-var units = "jj"  //Единици измерения ("kg" (kilogram), "hr" (hour), "km" (kilometer))
-var quantity = 10  //Количество единиц (1, 5, 10, 1000....)
+// ----------------------------------------
+// -----------Triangle ver1----------------
 
-switch (units) {
-    case "km":
-        console.log(quantity + " км це " + (quantity * 1000) + " м.");
-        break;
-    case "hr":
-        console.log(quantity + " гд це " + (quantity * 60) + " хв.");
-        break;
-    case "kg":
-        console.log(quantity + " кг це " + (quantity * 1000) + " грм.");
-        break;
-    default:
-        console.log("Чувааак, не гальмуй!");
-        break;
-}
+// function drawTriangle(){
+//     for (i = 0; i < 6; i++){
+//     let star = "";
+//     for (j = 0; j < i; j++){
+//         star += "*";
+//     }
+//     console.log(star);
+// }
+// }
+// drawTriangle()
+// -----------------------------------------
 
+function drawTriangle() {
+    let i = 1;
+    while (i <= 5) {
+      let stars = '';
+      let j = 0;
+      while (j < i) {
+        stars += '*';
+        j++;
+      }
+      console.log(stars);
+      i++;
+    }
+  }
+  
+  drawTriangle();
