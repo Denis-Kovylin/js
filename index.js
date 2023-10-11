4b31729a25c9c673672c21b4ded3abe97488dd12
Denis Kovylin <kovylin8519@gmail.com>
2023-10-12 02:12:32 +0300

    update

--- index.js
+++ index.js
@@ -1,44 +1,3 @@
-//----------------------------------------------------------------------
-//-------------------------Triangle ver1--------------------------------
-
-// function draw_triangle(triangleHeight, triangleSymbol){  
-//     let iterator = 0;
-//     let triangleRow = "";
-//     while(iterator < triangleHeight){
-//         triangleRow += triangleSymbol;
-//         console.log(triangleRow);
-//         iterator++
-//     }
-// }
-// draw_triangle(5, "*");
-
-//----------------------------------------------------------------------
-//-------------------------Triangle ver2--------------------------------
-
-// function draw_triangle(triangleHeight, triangleSymbol) {
-//     let triangleRow = "";
-//     for (let iterator = 0; iterator < triangleHeight; iterator++) {
-//         triangleRow += triangleSymbol;
-//         console.log(triangleRow);
-//     }
-// }
-// draw_triangle(5, "*");
-
-//-----------------------------------------------------------------------
-//----------------Sum of numbers not divisible by three------------------
-
-// var sumOfNumbers = 0;
-// var numberSwitcher = 0;
-
-// while (numberSwitcher < 101){
-//    if (numberSwitcher % 3 !== 0) {
-//     sumOfNumbers += numberSwitcher;
-//    }
-//    numberSwitcher++;
-//    continue;
-// }
-// console.log(sumOfNumbers);
-
 //-----------------------------------------------------------------------
 //-------------------------How old are you-------------------------------
 
@@ -77,18 +36,4 @@ else{
 
 
 
-// Написати функцію pow(x,y) яка буде приймати 2 цисла, перше це число яке ми будемо зводити у ступінь, друге у яку ступінь.
-
-// Наприклад pow(2,3) функція поверне значення 8.
-
-// Умови виконання ДЗ
-
-// Не використовувати **
-// Не використовувати функції або інші матеріали які ми не вивчали
-// Намагайтесь робити ДЗ самостійно та не дивитися будьякі підказки
-// Будь ласка, надайте наступні дані для перевірки вашої роботи:
-
-// Створіть гілку js_homework3.
-// Додати туди ваше домашне завдання
-// Вислати вашу гілку
 
