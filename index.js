d8e1e1ad76fc0e5499a456da0791a90d769fa472
Denis-Kovylin <122086120+Denis-Kovylin@users.noreply.github.com>
2023-10-15 01:50:41 +0300

    all tasks is done

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
+//----------------------------------------------------------------------
+//-------------------------Triangle ver1--------------------------------
+
+function draw_triangle(triangleHeight, triangleSymbol){  
+    let iterator = 0;
+    let triangleRow = "";
+    while(iterator < triangleHeight){
+        triangleRow += triangleSymbol;
+        console.log(triangleRow);
+        iterator++
+    }
+}
+draw_triangle(5, "*");
+
+//----------------------------------------------------------------------
+//-------------------------Triangle ver2--------------------------------
+
+function draw_triangle(triangleHeight, triangleSymbol) {
+    let triangleRow = "";
+    for (let iterator = 0; iterator < triangleHeight; iterator++) {
+        triangleRow += triangleSymbol;
+        console.log(triangleRow);
+    }
+}
+draw_triangle(5, "*");
+
 //-----------------------------------------------------------------------
-//-------------------------How old are you-------------------------------
+//----------------Sum of numbers not divisible by three------------------
+
+var sumOfNumbers = 0;
+var numberSwitcher = 0;
+
+while (numberSwitcher < 101){
+   if (numberSwitcher % 3 !== 0) {
+    sumOfNumbers += numberSwitcher;
+   }
+   numberSwitcher++;
+   continue;
+}
+console.log(sumOfNumbers);
+
+//-----------------------------------------------------------------------
+//-------------------------Function Exponentiation-----------------------
 
 var sum = 1;
 
